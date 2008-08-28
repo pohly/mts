4717510e328b22e799bcf7bcb442f19c8231c4a2
Patrick Ohly <patrick@charlie429.server4you.de>
2008-08-28 14:45:18 +0200

    initial import of Rolling Backup Module
    http://www.minitiddlyserver.com/viewtopic.php?f=1&t=135
    
    [New]Rolling Backup Module
    by streamrider on Sun Aug 17, 2008 7:59 pm
    
    Hello,
    
    I hacked together a rolling backup module. It saves minutely, hourly, daily, weekly, monthly and yearly backups. It's not really elegant, and after a year you're gonna have 155 copies of your TiddlyWiki from different weeks / months. After that it'll only grow by one file per year. It also copies the backup 6 times on each save, so depending on the size of your TiddlyWiki and your server's performance, it may add some time to the backup process every time you save. On my server it's barely noticeable.
    
    It used the original MTSAutoBackup Module as a blueprint and added a little bit of code to it.
    
    I attached a zip file containing the Module.
    
    Installation:
    
    * Unzip
    * put the unzipped directory into yourserverdir/MTS/Modules/
    * If on *nix, chmod to same rights as the rest of the modules if you want to / something doesn't work
    * IMPORTANT: copy the 'deactivate' - file from the Modules - Folder into the original MTSAutoBackup directory. This deactivates the old module. The two modules mustn't be used at the same time. You can also just create a new empty file with the name 'deactivate' in the MTSAutoBackup directory.
    
    http://www.minitiddlyserver.com/download/file.php?id=1&sid=dfe0465cd098daf4ed307d4d3cb7e5af

---
+++ MTS/Modules/MTSRollingAutoBackup/SaveBackup.php
@@ -0,0 +1,78 @@
+<?php
+
+    if ($clientRequest->doBackup ) {
+        // create new backup
+        createBackup($clientRequest->sourceFile, $sessionManager->user, $sessionManager->user."_auto.html");
+
+        // create copies of backup
+        createMultipleBackups($clientRequest->sourceFile, $sessionManager->user);
+    }
+
+    function createBackup($source,$backupSubDirName="noname", $backupName="noname.html") {
+        global $clientRequest, $serverInfo, $serverResponse;
+
+        $sourceName = $clientRequest->sourceName;
+        $backupDir = $serverInfo->BackupDirectory;
+
+        $mySourceBackupDir = $backupDir.$sourceName ."/";
+        $myBackupDir = $mySourceBackupDir . $backupSubDirName . "/";
+        $backupPath = $myBackupDir.$backupName;
+
+        if (is_dir($backupDir) === FALSE) {
+            if( mkdir($backupDir, 0755) === false )
+                $serverResponse->throwCriticalError("Could not create directory ($backupDir)");
+        }
+
+        if (is_dir($mySourceBackupDir) === FALSE) {
+            if( mkdir($mySourceBackupDir, 0755) === false )
+                $serverResponse->throwCriticalError("Could not create directory ($mySourceBackupDir)");
+        }
+
+        if (is_dir($myBackupDir) === FALSE) {
+            if( mkdir($myBackupDir, 0755) === false )
+                $serverResponse->throwCriticalError("Could not create directory ($myBackupDir)");
+        }
+
+        if ( copy($source, $backupPath))
+            $serverResponse->setString("backup",$backupName);
+        else
+            $serverResponse->throwCriticalError("Copy failed on backup : ($myBackupDir) ($source) ($backupName)");
+
+    }
+
+    function createMultipleBackups($source, $backupUserName="noname") {
+        global $clientRequest, $serverInfo, $serverResponse;
+
+        $sourceName = $clientRequest->sourceName;
+        $backupDir = $serverInfo->BackupDirectory;
+
+        $myBackupDir = $backupDir.$sourceName . "/" . $backupUserName . "/";
+
+        // subfolder => date() - string to save to
+        $subfolders = array(
+            'minutely' => 'i',
+            'hourly' =>   'H',
+            'daily'     => 'd_D',
+            'weekly'    => 'W',
+            'monthly'   => 'm_M',
+            'yearly'    => 'Y'
+        );
+
+        foreach (array_keys( $subfolders ) as $subfolder) {
+            if (!is_dir($myBackupDir . $subfolder)) {
+                   if( mkdir($myBackupDir . $subfolder, 0755) === false ) {
+                       $serverResponse->throwCriticalError("Could not create directory ($myBackupDir . $subfolder)");
+                   }
+            }
+        }
+
+        foreach ($subfolders as $subfolder => $dateString) {
+            $backupPathAndFilename = $myBackupDir . $subfolder . "/" . date($dateString) . '.html';
+            if ( copy($source, $backupPathAndFilename) ) {
+                $serverResponse->setString("backup",$backupUserName);
+            } else {
+                $serverResponse->throwCriticalError("Copy failed on backup : ($backupPathAndFilename)");
+            }
+        }
+    }
+?>
